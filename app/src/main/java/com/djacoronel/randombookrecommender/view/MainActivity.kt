7c83203a94c70c6cc44ec99040b4a092c950f6f7
Daniel Coronel <djacoronel@gmail.com>
2018-05-06 08:37:12 +0800

    Changed color scheme.

--- app/src/main/java/com/djacoronel/randombookrecommender/view/MainActivity.kt
+++ app/src/main/java/com/djacoronel/randombookrecommender/view/MainActivity.kt
@@ -31,9 +31,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         bookService = RetrofitHelper().getBookService()
-
         button.setOnClickListener { requestBooks() }
-
         setupBottomSheetBehavior()
     }
 
